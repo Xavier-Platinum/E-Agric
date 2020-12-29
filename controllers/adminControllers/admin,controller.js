0f185251b88180d0cd0ae9ac3193aa564c6c1c99
Xavier-Platinum <kwislawrencekwis@gmail.com>
2020-12-29 15:26:12 +0100

    #admin_approval_of_farmers

--- controllers/adminControllers/admin,controller.js
+++ controllers/adminControllers/admin,controller.js
@@ -14,5 +14,26 @@ module.exports = {
                 res.render("adminViews/all-farmers", { pageTitle, farmers });
             }
         })
+    },
+    approve_farmer: async(req, res) => {
+        let _id = req.params._id;
+        console.log(_id);
+        await Farmer.findOne({_id})
+        .then(async(approveFarmer) => {
+            approveFarmer.approved = true;
+            approveFarmer.save();
+            res.redirect("/admin/all-farmers");
+        })
+        .catch((err) => console.log(err));
+    },
+    decline_farmer: async(req, res) => {
+        const id = req.params._id;
+        await Farmer.findByIdAndDelete(id)
+        .then((declinedFarmer) => {
+            console.log(`${declinedFarmer} has been deleted`);
+            res.redirect("/admin/all-farmers");
+            return;
+        })
+        .catch((err) => console.log(err));
     }
 }
--- routes/adminRoutes/admin.routes.js
+++ routes/adminRoutes/admin.routes.js
@@ -1,12 +1,18 @@
 const router = require("express").Router();
 
 // Destructuring Routes
-const { index, all_farmersGet } = require("../../controllers/adminControllers/admin,controller");
+const { index, all_farmersGet, approve_farmer, decline_farmer } = require("../../controllers/adminControllers/admin,controller");
 
 // home route
 router.get("/",index);
 
 // All farmers
-router.get("/all-farmers", all_farmersGet)
+router.get("/all-farmers", all_farmersGet);
+
+// approve farmer
+router.get("/approveFarmer/:_id", approve_farmer);
+
+// decline farmer
+router.get("/declineFarmer/:_id", decline_farmer);
 
 module.exports = router;
