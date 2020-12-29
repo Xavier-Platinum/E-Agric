3bd0aee8d37736775f69f1c557f74f9a698b94ed
Xavier-Platinum <kwislawrencekwis@gmail.com>
2020-12-29 15:05:12 +0100

    #admin_query_farmers

--- controllers/adminControllers/admin,controller.js
+++ controllers/adminControllers/admin,controller.js
@@ -1,6 +1,18 @@
+const { Farmer } = require("../../models/farmersModel/farmers.model")
+
 module.exports = {
     index: (req, res) => {
         const pageTitle = "Admin"
         res.render("adminViews/admin", { pageTitle });
+    },
+    all_farmersGet: async (req, res) => {
+        await Farmer.find({approved: false})
+        .exec((err, farmers) => {
+            if (err) throw err;
+            else {
+                let pageTitle = "All Farmers";
+                res.render("adminViews/all-farmers", { pageTitle, farmers });
+            }
+        })
     }
 }
--- routes/adminRoutes/admin.routes.js
+++ routes/adminRoutes/admin.routes.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 
-
 // Destructuring Routes
-const { index } = require("../../controllers/adminControllers/admin,controller");
+const { index, all_farmersGet } = require("../../controllers/adminControllers/admin,controller");
 
 // home route
 router.get("/",index);
+
+// All farmers
+router.get("/all-farmers", all_farmersGet)
 
 module.exports = router;
